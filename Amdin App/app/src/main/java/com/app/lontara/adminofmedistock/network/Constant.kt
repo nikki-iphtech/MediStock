c01ad175e5f8b545554a209a2115bfe76a6f6c99
nikki-gupta-iphtech <nikki.gupta@iphtechnologies.com>
2025-06-12 14:39:15 +0530

    Initial commit

---
+++ Amdin App/app/src/main/java/com/app/lontara/adminofmedistock/network/Constant.kt
@@ -0,0 +1,3 @@
+package com.app.lontara.adminofmedistock.network
+
+const val BASE_URL = "put your baseUrl"
---
+++ User App/app/src/main/java/com/app/lontara/medicalstoreapp/API/Constants.kt
@@ -0,0 +1,3 @@
+package com.app.lontara.medicalstoreapp.API
+
+const val BASE_URL = "put your baseUrl"
